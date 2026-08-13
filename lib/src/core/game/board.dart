08d57a21c255f85b217285502d78cd84ca7e2c08
Andrii Kuchmenda <kuanwithxl@gmail.com>
2026-08-13 19:44:02 +0300

    implement player movement logic and update Board class structure

--- lib/src/core/game/board.dart
+++ lib/src/core/game/board.dart
@@ -1,15 +1,18 @@
+import 'package:messmath/src/core/game/direction.dart';
+
 class Board {
   final int width;
   final int height;
   final List<List<Cell>> cells;
 
-  Board(this.width, this.height)
-    : cells = List.generate(
-        height,
-        (y) => List.generate(width, (x) => Cell(x, y)),
+  Cell get playerCell => cells
+      .expand((row) => row)
+      .firstWhere(
+        (cell) => cell.type == CellType.player,
+        orElse: () => throw StateError('No player cell found in the board'),
       );
 
-  Board.fromCells(this.cells)
+  Board(this.cells)
     : width = cells.isNotEmpty ? cells[0].length : 0,
       height = cells.length;
 
@@ -25,6 +28,60 @@ class Board {
       throw RangeError('Cell coordinates out of bounds: ($x, $y)');
     }
     cells[y][x] = Cell(x, y, type: type, isWall: isWall);
+  }
+
+  void movePlayerPushing(Direction direction) {
+    final player = playerCell;
+    int newX = player.x;
+    int newY = player.y;
+
+    switch (direction) {
+      case Direction.up:
+        newY--;
+        break;
+      case Direction.down:
+        newY++;
+        break;
+      case Direction.left:
+        newX--;
+        break;
+      case Direction.right:
+        newX++;
+        break;
+    }
+
+    if (newX < 0 || newX >= width || newY < 0 || newY >= height) {
+      throw RangeError('Move out of bounds: ($newX, $newY)');
+    }
+
+    final targetCell = getCell(newX, newY);
+    if (targetCell.isWall) {
+      throw StateError('Cannot move to a wall cell: ($newX, $newY)');
+    }
+
+    if (direction == Direction.up || direction == Direction.down) {
+      int step = direction == Direction.up ? -1 : 1;
+      for (int y = newY; y >= 0 && y < height; y += step) {
+        final currentCell = getCell(newX, y);
+        if (currentCell.isWall) break;
+        if (y == newY) {
+          setCell(newX, y, CellType.player);
+        } else {
+          setCell(newX, y, getCell(newX, y - step).type);
+        }
+      }
+    } else {
+      int step = direction == Direction.left ? -1 : 1;
+      for (int x = newX; x >= 0 && x < width; x += step) {
+        final currentCell = getCell(x, newY);
+        if (currentCell.isWall) break;
+        if (x == newX) {
+          setCell(x, newY, CellType.player);
+        } else {
+          setCell(x, newY, getCell(x - step, newY).type);
+        }
+      }
+    }
   }
 }
 
