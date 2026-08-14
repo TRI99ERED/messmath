a33f865e20e47a17eff7ee11539a991087f90e3f
Andrii Kuchmenda <kuanwithxl@gmail.com>
2026-08-14 21:18:03 +0300

    add levels 2 to 15 with initial board configurations

--- lib/src/core/levels/level_loader.dart
+++ lib/src/core/levels/level_loader.dart
@@ -3,7 +3,20 @@ import 'package:messmath/src/core/levels/levels.dart';
 class LevelLoader {
   static final List<Level> levels = [
     level1,
-    // Add more levels here
+    level2,
+    level3,
+    level4,
+    level5,
+    level6,
+    level7,
+    level8,
+    level9,
+    level10,
+    level11,
+    level12,
+    level13,
+    level14,
+    level15,
   ];
 
   static Level loadLevel(int levelIndex) {
--- lib/src/core/levels/levels.dart
+++ lib/src/core/levels/levels.dart
@@ -17,3 +17,178 @@ final level1 = Level(
     ...P...
     '''),
 );
+
+final level2 = Level(
+  name: 'Push It',
+  initialBoard: Board.fromAsciiString('''
+    #########
+    #.1..[5=6]#
+    #.#+#####
+    #...P...#
+    #########
+    '''),
+);
+
+final level3 = Level(
+  name: 'The Swap',
+  initialBoard: Board.fromAsciiString('''
+    ########
+    #......#
+    #......#
+    #[2+]4[=]2.#
+    #..P...#
+    ########
+    '''),
+);
+
+final level4 = Level(
+  name: 'Tight Squeeze',
+  initialBoard: Board.fromAsciiString('''
+    ##.#####
+    ....[+8=9]
+    .#.#####
+    ...#####
+    ##1#####
+    #P.#####
+    '''),
+);
+
+final level5 = Level(
+  name: 'Big Numbers',
+  initialBoard: Board.fromAsciiString('''
+    ##########
+    #P1..[+9=19]
+    #.#0######
+    #........#
+    ##########
+    '''),
+);
+
+final level6 = Level(
+  name: 'Take Away',
+  initialBoard: Board.fromAsciiString('''
+    ##########
+    #........#
+    #........#
+    #.-4[2=2]..#
+    #P.......#
+    ##########
+    '''),
+);
+
+final level7 = Level(
+  name: 'In the Red',
+  initialBoard: Board.fromAsciiString('''
+    ##########
+    #........#
+    #........#
+    #P9[-]3[=]6-.#
+    #........#
+    ##########
+    '''),
+);
+
+final level8 = Level(
+  name: 'Times Tables',
+  initialBoard: Board.fromAsciiString('''
+    ##########
+    #........#
+    #........#
+    #[3]+[3=3]*[6]P#
+    #........#
+    ##########
+    '''),
+);
+
+final level9 = Level(
+  name: 'Divide and Conquer',
+  initialBoard: Board.fromAsciiString('''
+    ###...####
+    ###.=.####
+    ##[8].[2].[4]###
+    ###.#.####
+    #P../....#
+    ###...####
+    '''),
+);
+
+final level10 = Level(
+  name: 'Order of Ops',
+  initialBoard: Board.fromAsciiString('''
+    ############
+    #..........#
+    #..........#
+    #.[2]*[3]4[4=1]+.#
+    #..........#
+    #......P...#
+    ############
+    '''),
+);
+
+final level11 = Level(
+  name: 'Labyrinth',
+  initialBoard: Board.fromAsciiString('''
+    ....#.#..........
+    .#.......########
+    ..#.###.......###
+    #.#.[00-]1=[99]##....
+    #.#####.......###
+    P.1......####....
+    ########......###
+    '''),
+);
+
+final level12 = Level(
+  name: 'Double Trouble',
+  initialBoard: Board.fromAsciiString('''
+    ############
+    #..........#
+    #..........#
+    #[1]3/[1]+[64]=[2].#
+    #..........#
+    #....P.....#
+    ############
+    '''),
+);
+
+final level13 = Level(
+  name: 'Chain Reaction',
+  initialBoard: Board.fromAsciiString('''
+    #########
+    #.......#
+    #.#.....#
+    #..5....#
+    ##.5.[=2].#
+    #.*5....#
+    #P......#
+    #########
+    '''),
+);
+
+final level14 = Level(
+  name: 'House of Mirrors',
+  initialBoard: Board.fromAsciiString('''
+    ###############
+    #.............#
+    #.[9]9-[9]0[=]0[9]-99.#
+    #.............#
+    #.............#
+    #......P......#
+    ###############
+    '''),
+);
+
+final level15 = Level(
+  name: 'The Final Equation',
+  initialBoard: Board.fromAsciiString('''
+    ##...............##
+    ...................
+    .7#......-......#3.
+    #.#.............#.#
+    #...##.+.[=].*......#
+    #.#.............#.#
+    .1#......1......#5.
+    ...................
+    ##.......P.......##
+    '''),
+);
